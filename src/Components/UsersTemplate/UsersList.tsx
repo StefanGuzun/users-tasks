2b13e6f3bc9f0c10f11aa006770d35c9b7afe273
VladZara <vlad.zara.2028@icloud.com>
2022-07-07 23:02:19 +0300

    Added the Design to the project

---
+++ src/Components/UsersTemplate/UsersList.tsx
@@ -0,0 +1,13 @@
+import "../UsersStyle.css";
+
+const UsersList: React.FC = () => {
+  return (
+    <ul>
+      <div className="users">
+        <li style={{ marginLeft: "8px" }}>User 1</li>
+      </div>
+    </ul>
+  );
+};
+
+export default UsersList;
