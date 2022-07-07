6e3480dfd90a72816fcb3206271395572dd25f79
stefan <stefanguzun2005@gmail.com>
2022-07-07 23:18:39 +0300

    _

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Router,Routes, Route, Link} from "react-router-dom";
+
 
 function App() {
+
+  const testApi = ['john', 'anne', 'iulian']
+  const testApiPath = testApi.map((name) => <h1>{name}</h1>)
+  console.log(testApi);
+  
   return (
+    <Router location={''} navigator={undefined}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello world</h1>
+      <Routes>
+        {testApiPath}
+      </Routes>
     </div>
+    </Router>
   );
 }
 
