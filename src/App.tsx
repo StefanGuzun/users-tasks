e64af4b36ac9546d3c31eca35014063ff23171c4
stefan <stefanguzun2005@gmail.com>
2022-07-07 23:19:36 +0300

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,11 @@ import { BrowserRouter, Router,Routes, Route, Link} from "react-router-dom";
 
 
 function App() {
-
-  const testApi = ['john', 'anne', 'iulian']
-  const testApiPath = testApi.map((name) => <h1>{name}</h1>)
-  console.log(testApi);
-  
   return (
-    <Router location={''} navigator={undefined}>
     <div className="App">
       <h1>hello world</h1>
-      <Routes>
-        {testApiPath}
-      </Routes>
+
     </div>
-    </Router>
   );
 }
 
