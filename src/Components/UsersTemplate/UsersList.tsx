b697faf2c6d9d63a61aeb42512b80f230f2543d9
stefan <stefanguzun2005@gmail.com>
2022-07-08 10:22:37 +0300

    i modified the style

--- src/Components/UsersTemplate/UsersList.tsx
+++ src/Components/UsersTemplate/UsersList.tsx
@@ -2,11 +2,13 @@ import "../UsersStyle.css";
 
 const UsersList: React.FC = () => {
   return (
+    <div>
     <ul>
       <div className="users">
         <li style={{ marginLeft: "8px" }}>User 1</li>
       </div>
     </ul>
+    </div>
   );
 };
 
