be27a566147fe6d283ae2c25105ffd4adfeac013
Iulian <iulian.gonta13@gmail.com>
2022-07-09 16:47:49 +0300

    ...

--- src/Components/UsersTemplate/UsersTemplate.tsx
+++ src/Components/UsersTemplate/UsersTemplate.tsx
@@ -1,7 +1,6 @@
-import React,  { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../UsersStyle.css";
 import UsersTasks from "../UsersTasks/UsersTasks";
-import UsersList from "./UsersList";
 import { getTasks, getUsers } from "../../Api";
 
 const UserTemplate: React.FC = () => {
@@ -9,40 +8,46 @@ const UserTemplate: React.FC = () => {
   const [users, setUsers] = useState([])
   const [tasks, setTasks] = useState<any>([])
 
-  interface Users{
+  interface Users {
     name: string;
   }
-  interface Tasks{
+  interface Tasks {
     title: string;
   }
-useEffect(() => {
+  useEffect(() => {
     getTasks()
-    .then(res => setTasks(res.data))
+      .then(res => setTasks(res.data))
     getUsers()
-    .then(res => setUsers(res.data))
-}, [])
+      .then(res => setUsers(res.data))
+  }, [])
 
   return (
-      <div className="container">
-        <div className="gradientBorder"/>
-        <div className="titleCont">
+    <div className="container">
+      <div className="gradientBorder" />
+      <div className="titleCont">
         <div className="title">
         </div>
-          <h1>Onboarding Tracker</h1>
+        <h1>Onboarding Tracker</h1>
+      </div>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <div className="containerUsers">
+          <h1 style={{ marginLeft: "33px", fontFamily: "Comic Neue" }}>
+            Users
+          </h1>
+          {users.map((users: Users) => <div>
+            <ul>
+              <div className="users">
+                <li style={{ marginLeft: "8px" }}>{users.name}</li>
+              </div>
+            </ul>
+          </div>)}
         </div>
-        <div style={{ display: "flex", justifyContent: "space-between" }}>
-          <div className="containerUsers">
-            <h1 style={{ marginLeft: "33px", fontFamily: "Comic Neue" }}>
-              Users
-            </h1>
-            {users.map((users: Users) => <UsersList users={users}/>)}
-          </div>
-          <div>
+        <div>
           <h1 style={{ marginLeft: "33px", fontFamily: "Comic Neue" }}>Tasks</h1>
-          {tasks.map((tasks: Tasks) => <UsersTasks task={tasks}/>)}
-          </div>
+          {tasks.map((tasks: Tasks) => <UsersTasks task={tasks} />)}
         </div>
       </div>
+    </div>
   );
 };
 
